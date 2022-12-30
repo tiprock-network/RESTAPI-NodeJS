46148b1ef2064b23618c3a8fe5c48fc9ac4fff67
Theophilus <lincolntheop@gmail.com>
2022-12-30 23:32:21 +0300

    Updated GET,POST,PUT,DELETE Requests

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const http=require('http')
-const products=require('./data/products')//our data
-const {getProducts,getProduct}=require('./controller/productController')
+const products=require('./data/products')//our data json file (not necessary here)
+const {getProducts,getProduct,createProduct,updateProduct,deleteProduct}=require('./controller/productController')
 const server=http.createServer((req,res)=>{
-    if(req.url=='/api/products' && req.method=='GET'){
+    if(req.url==='/api/products' && req.method==='GET'){
         getProducts(req,res)
-    }else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method=='GET'){
+    }else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method==='GET'){
         const id=req.url.split('/')[3]
         getProduct(req,res,id)
-    }else{
+    }else if(req.url==='/api/products' && req.method==='POST'){
+        createProduct(req,res)
+    }else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method==='PUT'){
+        const id=req.url.split('/')[3]
+        updateProduct(req,res,id)
+    }else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method==='DELETE'){
+        const id=req.url.split('/')[3]
+        deleteProduct(req,res,id)
+    }
+    else{
         res.writeHead(404,{'Content-Type':'application/json'})
         res.end(JSON.stringify({message:'Route Not Found'}))
     }
